3dfda604b9ab6d27aad24e947f60b757cfe28f59
shidqiiii <shidqidaffa@gmail.com>
2024-09-29 00:55:19 +0700

    feat: setup and create register user api

---
+++ src/validation/user-validation.ts
@@ -0,0 +1,9 @@
+import { z, ZodType } from "zod";
+
+export class UserValidation {
+    static readonly REGISTER: ZodType = z.object({
+        username: z.string().min(1),
+        password: z.string().min(1),
+        name: z.string().min(1),
+    })
+}
