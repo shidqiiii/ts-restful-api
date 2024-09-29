b51d4c895fef8a6da35911a47aca9e72380b57a3
shidqiiii <shidqidaffa@gmail.com>
2024-09-29 11:37:30 +0700

    feat: create login user api

--- src/model/user-model.ts
+++ src/model/user-model.ts
@@ -3,12 +3,18 @@ import { User } from "@prisma/client"
 export type UserResponse = {
     username: string,
     name: string,
-    token?: string
+    token?: string,
 }
 
 export type CreateUserRequest = {
     username: string,
     name: string
+    password: string,
+}
+
+
+export type LoginUserRequest = {
+    username: string,
     password: string,
 }
 
