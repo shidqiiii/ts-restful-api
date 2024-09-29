901f092e1d3c0cb847a4597d07164f23647f0446
shidqiiii <shidqidaffa@gmail.com>
2024-09-29 12:59:01 +0700

    feat: create get current user api

--- test/user.test.ts
+++ test/user.test.ts
@@ -77,4 +77,33 @@ describe('POST /api/users/login', () => {
         expect(response.status).toBe(401)
         expect(response.body.errors).toBeDefined()
     })
+
+
+})
+
+describe('GET /api/users/current', () => {
+    beforeEach(async () => {
+        await UserTest.create()
+    })
+
+    afterEach(async () => {
+        await UserTest.delete()
+    })
+
+    it('should be able to get current user', async () => {
+        const response = await supertest(web).get('/api/users/current').set("X-API-TOKEN", "test")
+
+        logger.debug(response.body)
+        expect(response.status).toBe(200)
+        expect(response.body.data.username).toBe('test')
+        expect(response.body.data.name).toBe('test')
+    })
+
+    it('should reject user if token is invalid', async () => {
+        const response = await supertest(web).get('/api/users/current').set("X-API-TOKEN", "salah")
+
+        logger.debug(response.body)
+        expect(response.status).toBe(401)
+        expect(response.body.errors).toBeDefined()
+    })
 })
